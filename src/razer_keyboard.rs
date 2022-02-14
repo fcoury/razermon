de61c7e1642f3af1cb9074ca0eb43e779853e301
Frank Fenton <echelon.iiii@gmail.com>
2022-02-15 01:35:02 +1100

    chore: rename keyboards so they dont all begin with razer

--- src/razer_keyboard.rs
+++ src/razer_keyboard.rs
@@ -7,168 +7,198 @@ use strum::FromRepr;
 #[associated(Type = RazerDeviceConnectInfo)]
 pub enum RazerKeyboardKind {
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowUltimate2012 = 0x010d,
+    BlackwidowUltimate2012 = 0x010d,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowStealthEdition = 0x010e,
+    BlackwidowStealthEdition = 0x010e,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerAnansi = 0x010f,
+    Anansi = 0x010f,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerNostromo = 0x0111,
+    Nostromo = 0x0111,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerOrbweaver = 0x0113,
+    Orbweaver = 0x0113,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowUltimate2013 = 0x011a,
+    BlackwidowUltimate2013 = 0x011a,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowStealth = 0x011b,
+    BlackwidowStealth = 0x011b,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowTe2014 = 0x011c,
+    BlackwidowTe2014 = 0x011c,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerTartarus = 0x0201,
+    Tartarus = 0x0201,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerDeathstalkerExpert = 0x0202,
+    DeathstalkerExpert = 0x0202,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowChroma = 0x0203,
+    BlackwidowChroma = 0x0203,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerDeathstalkerChroma = 0x0204,
+    DeathstalkerChroma = 0x0204,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeStealth = 0x0205,
+    BladeStealth = 0x0205,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerOrbweaverChroma = 0x0207,
+    OrbweaverChroma = 0x0207,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerTartarusChroma = 0x0208,
+    TartarusChroma = 0x0208,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowChromaTe = 0x0209,
+    BlackwidowChromaTe = 0x0209,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeQhd = 0x020f,
+    BladeQhd = 0x020f,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeProLate2016 = 0x0210,
+    BladeProLate2016 = 0x0210,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowOverwatch = 0x0211,
+    BlackwidowOverwatch = 0x0211,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowUltimate2016 = 0x0214,
+    BlackwidowUltimate2016 = 0x0214,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowXChroma = 0x0216,
+    BlackwidowXChroma = 0x0216,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowXUltimate = 0x0217,
+    BlackwidowXUltimate = 0x0217,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowXChromaTe = 0x021a,
+    BlackwidowXChromaTe = 0x021a,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerOrnataChroma = 0x021e,
+    OrnataChroma = 0x021e,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerOrnata = 0x021f,
+    Ornata = 0x021f,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeStealthLate2016 = 0x0220,
+    BladeStealthLate2016 = 0x0220,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowChromaV2 = 0x0221,
+    BlackwidowChromaV2 = 0x0221,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeLate2016 = 0x0224,
+    BladeLate2016 = 0x0224,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladePro2017 = 0x0225,
+    BladePro2017 = 0x0225,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerHuntsmanElite = 0x0226,
+    HuntsmanElite = 0x0226,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerHuntsman = 0x0227,
+    Huntsman = 0x0227,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowElite = 0x0228,
+    BlackwidowElite = 0x0228,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerCynosaChroma = 0x022a,
+    CynosaChroma = 0x022a,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerTartarusV2 = 0x022b,
+    TartarusV2 = 0x022b,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerCynosaChromaPro = 0x022c,
+    CynosaChromaPro = 0x022c,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeStealthMid2017 = 0x022d,
+    BladeStealthMid2017 = 0x022d,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladePro2017Fullhd = 0x022f,
+    BladePro2017Fullhd = 0x022f,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeStealthLate2017 = 0x0232,
+    BladeStealthLate2017 = 0x0232,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade2018 = 0x0233,
+    Blade2018 = 0x0233,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladePro2019 = 0x0234,
+    BladePro2019 = 0x0234,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowLite = 0x0235,
+    BlackwidowLite = 0x0235,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowEssential = 0x0237,
+    BlackwidowEssential = 0x0237,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeStealth2019 = 0x0239,
+    BladeStealth2019 = 0x0239,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade2019Adv = 0x023a,
+    Blade2019Adv = 0x023a,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade2018Base = 0x023b,
+    Blade2018Base = 0x023b,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerCynosaLite = 0x023f,
+    CynosaLite = 0x023f,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade2018Mercury = 0x0240,
+    Blade2018Mercury = 0x0240,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidow2019 = 0x0241,
+    Blackwidow2019 = 0x0241,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerHuntsmanTe = 0x0243,
+    HuntsmanTe = 0x0243,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeMid2019Mercury = 0x0245,
+    BladeMid2019Mercury = 0x0245,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade2019Base = 0x0246,
+    Blade2019Base = 0x0246,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeStealthLate2019 = 0x024a,
+    BladeStealthLate2019 = 0x024a,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeProLate2019 = 0x024c,
+    BladeProLate2019 = 0x024c,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeStudioEdition2019 = 0x024d,
+    BladeStudioEdition2019 = 0x024d,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(3), usage: Some(1), usage_page: Some(0x0C)})]
-    RazerBlackwidowV3 = 0x024e,
+    BlackwidowV3 = 0x024e,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeStealthEarly2020 = 0x0252,
+    BladeStealthEarly2020 = 0x0252,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade15Adv2020 = 0x0253,
+    Blade15Adv2020 = 0x0253,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeEarly2020Base = 0x0255,
+    BladeEarly2020Base = 0x0255,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeProEarly2020 = 0x0256,
+    BladeProEarly2020 = 0x0256,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerHuntsmanMini = 0x0257,
+    HuntsmanMini = 0x0257,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(3), usage: Some(1), usage_page: Some(0x0C)})]
-    RazerBlackwidowV3Mini = 0x0258,
+    BlackwidowV3Mini = 0x0258,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBladeStealthLate2020 = 0x0259,
+    BladeStealthLate2020 = 0x0259,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowV3ProWired = 0x025a,
+    BlackwidowV3ProWired = 0x025a,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(3), usage: Some(1), usage_page: Some(0x0C)})]
-    RazerBlackwidowV3ProWireless = 0x025c,
+    BlackwidowV3ProWireless = 0x025c,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerOrnataV2 = 0x025d,
+    OrnataV2 = 0x025d,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerCynosaV2 = 0x025e,
+    CynosaV2 = 0x025e,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(3), usage: Some(1), usage_page: Some(0x0C)})]
-    RazerHuntsmanV2Analog = 0x0266,
+    HuntsmanV2Analog = 0x0266,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerHuntsmanMiniJp = 0x0269,
+    HuntsmanMiniJp = 0x0269,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBook2020 = 0x026a,
+    Book2020 = 0x026a,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(3), usage: Some(1), usage_page: Some(0x0C)})]
-    RazerHuntsmanV2TKL = 0x026b,
+    HuntsmanV2TKL = 0x026b,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(3), usage: Some(1), usage_page: Some(0x0C)})]
-    RazerHuntsmanV2 = 0x026c,
+    HuntsmanV2 = 0x026c,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade15AdvEarly2021 = 0x026d,
+    Blade15AdvEarly2021 = 0x026d,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade15BaseEarly2021 = 0x026f,
+    Blade15BaseEarly2021 = 0x026f,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade142021 = 0x0270,
+    Blade142021 = 0x0270,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(3), usage: Some(1), usage_page: Some(0x0C)})]
-    RazerBlackwidowV3MiniWireless = 0x0271,
+    BlackwidowV3MiniWireless = 0x0271,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade15AdvMid2021 = 0x0276,
+    Blade15AdvMid2021 = 0x0276,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlade17ProMid2021 = 0x0279,
+    Blade17ProMid2021 = 0x0279,
     #[assoc_const(RazerDeviceConnectInfo {interface_number: Some(2), usage: Some(2), usage_page: Some(1)})]
-    RazerBlackwidowV3Tk = 0x0a24,
+    BlackwidowV3Tk = 0x0a24,
 }
 
 impl RazerKeyboardKind {
     pub fn is_blade(&self) -> bool {
+        use RazerKeyboardKind::*;
         match self {
-            &RazerKeyboardKind::RazerBladeStealth
-            | &RazerKeyboardKind::RazerBladeStealthLate2016 => true,
+            BladeStealth
+            | BladeStealthLate2016
+            | BladeProLate2016
+            | Blade2018
+            | Blade2018Mercury
+            | Blade2018Base
+            | Blade2019Adv
+            | BladeMid2019Mercury
+            | BladeStudioEdition2019
+            | BladeQhd
+            | BladeLate2016
+            | BladeStealthMid2017
+            | BladeStealthLate2017
+            | BladeStealth2019
+            | BladePro2017
+            | BladePro2017Fullhd
+            | Blade2019Base
+            | BladeStealthLate2019
+            | BladePro2019
+            | BladeProLate2019
+            | BladeStealthEarly2020
+            | BladeStealthLate2020
+            | BladeProEarly2020
+            | Book2020
+            | Blade15Adv2020
+            | BladeEarly2020Base
+            | Blade15AdvEarly2021
+            | Blade15AdvMid2021
+            | Blade15BaseEarly2021
+            | Blade17ProMid2021
+            | Blade142021 => true,
             _ => false,
         }
     }
