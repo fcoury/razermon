80d8989f5b98699fd60622abe1461ec2630ba761
Felipe Coury <felipe.coury@gmail.com>
2022-10-07 13:46:05 -0300

    feat: initial tray menu works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,48 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .system_tray(
+            SystemTray::new().with_menu(
+                SystemTrayMenu::new()
+                    .add_item(CustomMenuItem::new("no_devices", "No Devices Found"))
+                    .add_item(CustomMenuItem::new("quit", "Quit")),
+            ),
+        )
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window = app.get_window("main").unwrap();
+                window.show().unwrap();
+                window.set_focus().unwrap();
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+                let item_handle = app.tray_handle().get_item(&id);
+                match id.as_str() {
+                    "no_devices" => {
+                        eprintln!("No devices clicked");
+                    }
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        })
+        // .invoke_handler(tauri::generate_handler![
+        //     cmd::log_operation,
+        //     cmd::perform_request,
+        //     menu_toggle,
+        // ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
