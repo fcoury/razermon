7bdabcf5792a433a40bc9e33860189440bdec469
Felipe Coury <felipe.coury@gmail.com>
2022-10-11 18:48:20 -0300

    feat: skeleton front-end implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import Home from "./pages/Home";
+import theme from "./theme";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ChakraProvider theme={theme}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </ChakraProvider>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import { Box } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <Box p={5}>
+    </Box>
+  );
+}
