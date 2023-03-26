9412fe671681fe13bdf346309b0c701c7f0b336a
Felipe Coury <felipe.coury@gmail.com>
2023-03-26 00:48:04 -0300

    feat: build script

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+BASE_PATH=$(dirname "$0")
+LEVEL=
+# if [ "$1" = "--debug" ]; then
+#   LEVEL=" --debug"
+# fi
+
+export APPLE_SIGNING_IDENTITY="Developer ID Application: Felipe Coury (UXWHD5F6F4)"
+export APPLE_CERTIFICATE=$(cat "$HOME/Documents/certs/razermon-base64.txt")
+export APPLE_CERTIFICATE_PASSWORD=$(op item get razermon-cert-pwd --fields password)
+
+if [ -z "${LEVEL}" ]; then
+  echo "Building release version"
+  ./tauri/tooling/cli/node/tauri.js build --verbose --target aarch64-apple-darwin
+else
+  echo "Building debug version"
+  ./tauri/tooling/cli/node/tauri.js build --debug --verbose --target aarch64-apple-darwin
+fi
